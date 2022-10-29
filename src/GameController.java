cd56a06ff6dcdae935b5ebf848bbc472cb91ef62
jharwood-63 <83321059+jharwood-63@users.noreply.github.com>
2022-10-29 10:56:20 -0600

    Complete main functionality

--- src/GameController.java
+++ src/GameController.java
@@ -1,18 +1,78 @@
 import GamePlay.Display;
 import GamePlay.Hangman;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.Scanner;
 
 public class GameController {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Hangman hangman = new Hangman();
         Display display = new Display();
 
+        boolean continueGame = true;
+        int numWords = 10;
+//        int numWordsCorrect = 0;
+        boolean useGallows;
+        String selectedWord;
+        int numBodyParts;
+        int numGuessesMade;
+        int numCorrect;
+        int numIncorrect;
+
         display.displayWelcome();
-        String selectedWord = hangman.selectWord();
+        while (continueGame && numWords > 0) {
+            selectedWord = hangman.selectWord();
+            display.initializeGuessedWord(selectedWord);
+            display.displayNumLetters(selectedWord);
 
+            useGallows = false;
+            numBodyParts = 6;
+            numGuessesMade = 0;
+            numIncorrect = 0;
+            numCorrect = 0;
+            numWords--;
 
+            System.out.print("Would you like to play with gallows? Enter y or n: ");
+            String chooseGallows = scanner.nextLine();
+            if (chooseGallows.equalsIgnoreCase("y")) {
+                useGallows = true;
+            }
+
+            while (!display.isWordGuessed() && numBodyParts > 0) {
+                System.out.print("Please guess a letter: ");
+                String guessedLetter = scanner.nextLine();
+                if (!display.alreadyGuessed(guessedLetter)) {
+                    numGuessesMade++;
+                    boolean isInWord = display.updateGuessedWord(selectedWord, guessedLetter);
+                    if (isInWord) {
+                        numCorrect++;
+                    } else {
+                        //update the gallows
+                        if (useGallows) {
+                            numBodyParts--;
+                        }
+
+                        numIncorrect++;
+                    }
+                }
+                else {
+                    System.out.println("You have already guessed " + guessedLetter + "! Please try again!");
+                }
+                display.displayNumGuesses(numGuessesMade, numIncorrect, numCorrect);
+            }
+
+            display.displayResult(selectedWord, numGuessesMade, !(numBodyParts == 0));
+
+            System.out.print("Would you like to continue playing? Enter y or n: ");
+            String continuePrompt = scanner.nextLine();
+            continueGame = continuePrompt.equalsIgnoreCase("y");
+        }
+
+        if (numWords == 0) {
+            System.out.println("You have attempted to guess all of my words!");
+        }
+        else {
+            System.out.println("Thanks for playing!");
+        }
     }
 }
--- src/GamePlay/Display.java
+++ src/GamePlay/Display.java
@@ -1,9 +1,104 @@
 package GamePlay;
 
-public class Display {
+import java.util.ArrayList;
 
+public class Display {
+    private ArrayList<Character> guessedWord;
+    private final ArrayList<String> incorrectLetters = new ArrayList<>();
+    private final char[] bodyParts = {'O', '|', '/', '\\', '/', '\\'};
 
     public void displayWelcome() {
         System.out.println("Welcome to Hangman!");
+    }
+
+    public void displayNumLetters(String selectedWord) {
+        System.out.println("Word Size: " + selectedWord.length());
+    }
+
+    public void initializeGuessedWord(String selectedWord) {
+        guessedWord = new ArrayList<>();
+        for (int i = 0; i < selectedWord.length(); i++) {
+            guessedWord.add('_');
+        }
+    }
+
+    public boolean updateGuessedWord(String selectedWord, String guessedLetter) {
+        int numLettersCorrect = 0;
+        boolean isInWord = false;
+
+        if (selectedWord.contains(guessedLetter)) {
+            for (int i = 0; i < guessedWord.size(); i++) {
+                if ((i + 1) < guessedWord.size()) {
+                    if (guessedLetter.equals(selectedWord.substring(i, i + 1))) {
+                        guessedWord.set(i, guessedLetter.charAt(0));
+                        numLettersCorrect++;
+                    }
+                } else {
+                    if (guessedLetter.equals(selectedWord.substring(i))) {
+                        guessedWord.set(i, guessedLetter.charAt(0));
+                        numLettersCorrect++;
+                    }
+                }
+            }
+
+            if (numLettersCorrect == 1) {
+                System.out.println("There is " + numLettersCorrect + " " + guessedLetter + " in the word");
+            } else {
+                System.out.println("There are " + numLettersCorrect + " " + guessedLetter + "s in the word");
+            }
+
+            isInWord = true;
+        } else {
+            System.out.println("The letter " + guessedLetter + " is not in the word");
+            incorrectLetters.add(guessedLetter);
+        }
+
+        displayIncorrectLetters();
+        displayGuessedWord();
+        return isInWord;
+    }
+
+    public boolean alreadyGuessed(String guessedLetter) {
+        return guessedWord.contains(guessedLetter.charAt(0)) || incorrectLetters.contains(guessedLetter);
+    }
+
+    private void displayGuessedWord() {
+        for (Character letter : guessedWord) {
+            System.out.print(letter + " ");
+        }
+        System.out.println();
+    }
+
+    public boolean isWordGuessed() {
+        for (char letter : guessedWord) {
+            if (letter == '_') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void displayIncorrectLetters() {
+        System.out.print("Incorrect Letters: ");
+        for (String letter : incorrectLetters) {
+            System.out.print(letter + " ");
+        }
+        System.out.println();
+    }
+
+    public void displayResult(String selectedWord, int numGuesses, boolean guessedWord) {
+        if (guessedWord) {
+            System.out.println("Congratulations! You guessed the word " + selectedWord + " correctly!");
+            System.out.println("You saved Bowly's life! And it only took you " + numGuesses + " guesses!");
+        }
+        else {
+            System.out.println("Oh no! Bowly was hanged for his crimes!");
+            System.out.println("The word was " + selectedWord + "!");
+        }
+    }
+
+    public void displayNumGuesses(int numGuesses, int numIncorrect, int numCorrect) {
+        System.out.println("Guesses: " + numGuesses + " Correct: " + numCorrect + " Incorrect: " + numIncorrect);
     }
 }
